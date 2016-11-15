b410b4fff8b3bf4648b25292244dae88743c6b8f
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2016-11-15 18:43:31 +0100

    Initial commit with BallGame after two hours of first time touching unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text scoreText;
+	public int highscore;
+	private Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidBody.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider collidee) {
+		highscore += 100;
+		scoreText.text = "Score: " + highscore;
+	}
+}
---
+++ Assets/Scripts/TextAbsolutizer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextAbsolutizer : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = this.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = player.transform.position + offset;
+	}
+}
